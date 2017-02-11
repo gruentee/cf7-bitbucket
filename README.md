4917c4b0905ac26add6a8e780a8b66d41232ee7b
Constantin <ckraft@smail.uni-koeln.de>
2017-02-11 15:38:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CF7 BitBucket
+A [Wordpress](https://wordpress.org) plugin to create BitBucket issues from [Contact Form 7](https://contactform7
+.com) submissions.
+
+## Features
+
+## Installation
+
+### Requirements
+
+## ToDo
+
+## License
---
+++ cf7-bitbucket.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: CF7 Bitbucket
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Create BitBucket issues from Contact Form 7 form submissions
+Version: 1.0
+Author: connzen
+Author URI: https://websolutions.koeln
+License: A "Slug" license name e.g. GPL2
+*/
+
